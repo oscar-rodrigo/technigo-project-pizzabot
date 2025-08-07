e57bc68333641a526ec6af9508dd57862e702d27
Oscar Flores <oscar@talkingtome.se>
2025-08-07 23:06:12 +0200

    Added meny and subchoice.

--- code/script.js
+++ code/script.js
@@ -8,9 +8,23 @@ alert(
 
 // Step 2 - Food choice
 // Your code goes here
+let userChoice = prompt(
+  "What would you like to eat?\n\n We have these things on the menu: \n1. Pizza\n2. Kebab\n3. Rice "
+)
+
+alert(
+  `Nice, ${userChoice} is a good choice!`
+)
 
 // Step 3 - Subtype choice
 // Your code goes here
+let userSubChoice
+
+if (userChoice === "Pizza") {
+  userSubChoice = prompt("What type of pizza do you want, Italian or American?")
+} else if (userChoice === "Kebab") {
+  userSubChoice = prompt("What type of Kebab would you like, Swedish or Iraqi?")
+}
 
 // Step 4 - Age
 // Your code goes here
